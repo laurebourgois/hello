32e59cf4130220ee468f077fe388faafb1f46347
Laure Bourgois <bourgoislaure@gmail.com>
2016-07-04 14:14:19 +0200

    second commit

---
+++ EjbTest/ejbModule/objis/Test.java
@@ -0,0 +1,22 @@
+package objis;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Test
+ */
+@Stateless
+@LocalBean
+public class Test {
+
+    /**
+     * Default constructor. 
+     */
+    public Test() {
+    	
+    	System.out.println(" hello ");
+        // TODO Auto-generated constructor stub
+    }
+
+}
